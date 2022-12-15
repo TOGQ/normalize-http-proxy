7f078550f2f472ccfc527ffba651daac7520e5a8
TOGQ <92612877+TOGQ@users.noreply.github.com>
2022-12-15 09:37:19 +0800

    Update factory.go

--- server/factory.go
+++ server/factory.go
@@ -5,7 +5,7 @@ import "fmt"
 func NewBaiduServer(address, listen string, verbose bool) *Server {
 	upstream := NewUpstream(
 		WithAddress(fmt.Sprintf("%s:443", address)),
-		WithHeaders(Headers{"X-T5-Auth": "ZjQxNDIh"}),
+		WithHeaders(Headers{"X-T5-Auth": "ZjQxNDIh", "User-Agent": "okhttp/3.11.0 Dalvik/2.1.0 (Linux; U; Android 11; Redmi K30 5G Build/RKQ1.200826.002) baiduboxapp/11.0.5.12 (Baidu; P1 11)"}),
 	)
 	return &Server{
 		Listen:        listen,
